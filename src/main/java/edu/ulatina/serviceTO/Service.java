6cfee4895908af498f5ae9ba10fc9e92e7da7abf
Nwitlyck <davidsegura118@hotmail.com>
2023-06-14 18:34:45 -0600

    Fist commit

---
+++ src/main/java/edu/ulatina/serviceTO/Service.java
@@ -0,0 +1,46 @@
+package edu.ulatina.serviceTO;
+
+import java.sql.*;
+
+/**
+ * @author PegasusTeam
+ */
+public class Service {
+
+    protected Connection conn = null;
+
+    public Service() {
+    }
+
+    protected Connection getConnection() throws Exception {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectpegasus?serverTimezone=UTC", "root", "Root");
+        //make sure u have the data base up and change ur user and password
+
+        return conn;
+    }
+
+    protected void close(Connection toClose) throws Exception {
+        if (toClose != null && !toClose.isClosed()) {
+            toClose.close();
+            toClose = null;
+        }
+    }
+
+    protected void close(PreparedStatement toClose) throws Exception {
+        if (toClose != null && !toClose.isClosed()) {
+            toClose.close();
+            toClose = null;
+        }
+    }
+
+    protected void close(ResultSet toClose) throws Exception {
+        if (toClose != null && !toClose.isClosed()) {
+            toClose.close();
+            toClose = null;
+        }
+    }
+
+}
